6088c5a823842b4a7ff6d5a88fa5cb066e469dd1
Pau Valverde <pvalverdemolina@gmail.com>
2021-11-14 08:55:34 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Pau Valverde Molina on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
